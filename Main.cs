67d5094679781d2f0aebc3f4b951c65d0ca7d0e1
blazethrower320 <blazethrower320@gmail.com>
2022-04-23 22:25:59 -0500

    Release
    Yeah... Another Release. Idk what to put here

---
+++ Main.cs
@@ -0,0 +1,81 @@
+﻿using BTMovementTracker.Helpers;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using ShimmyMySherbet.DiscordWebhooks.Embeded;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace BTMovementTracker
+{
+    public class Main : RocketPlugin<MovementTrackerConfiguration>
+    {
+        public static Main Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("#############################################", ConsoleColor.Yellow);
+            Logger.Log("###           BTMovementTracker           ###", ConsoleColor.Yellow);
+            Logger.Log("###   Plugin Created By blazethrower320   ###", ConsoleColor.Yellow);
+            Logger.Log("###            Join my Discord:           ###", ConsoleColor.Yellow);
+            Logger.Log("###     https://discord.gg/YsaXwBSTSm     ###", ConsoleColor.Yellow);
+            Logger.Log("#############################################", ConsoleColor.Yellow);
+            //
+            //UnturnedPlayerEvents.OnPlayerUpdatePosition += OnPlayerUpdatePosition;
+            U.Events.OnPlayerConnected += OnPlayerConnected;
+            U.Events.OnPlayerDisconnected += OnPlayerDisconnected;
+        }
+
+        private void OnPlayerDisconnected(UnturnedPlayer player)
+        {
+            StopCoroutine(CheckPosition(player));
+        }
+
+        private void OnPlayerConnected(UnturnedPlayer player)
+        {
+            StartCoroutine(CheckPosition(player));
+        }
+        public IEnumerator CheckPosition(UnturnedPlayer player)
+        {
+            while (true && Provider.clients.Contains(player.SteamPlayer()))
+            {
+                yield return new WaitForSeconds(30f);
+                if (Main.Instance.Configuration.Instance.LogInConsole)
+                {
+                    Logger.Log(player.CharacterName + " Position Update: " + player.Position);
+                }
+                if (Main.Instance.Configuration.Instance.useWebhook)
+                {
+                    ThreadHelper.RunSynchronously(() => {
+                        WebhookMessage PositionUpdate = new WebhookMessage()
+                        .PassEmbed()
+                        .WithTitle("MovementTracker - Position Update")
+                        .WithColor(EmbedColor.BlueViolet)
+                        .WithURL("https://steamcommunity.com/profiles/" + player.CSteamID)
+                        .WithTimestamp(DateTime.Now)
+                        .WithField("Username", player.CharacterName)
+                        .WithField("SteamId", player.CSteamID.ToString())
+                        .WithField("New Position", player.Position.ToString())
+                        .WithField("Delay", Main.Instance.Configuration.Instance.TrackDelay.ToString())
+                        .Finalize();
+                        DiscordWebhookService.PostMessageAsync(Main.Instance.Configuration.Instance.WebhookURL, PositionUpdate);
+                    });
+                }
+            }
+        }
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= OnPlayerConnected;
+            Logger.Log("BTMovementTracker Unloaded");
+        }
+    }
+}
---
+++ MovementTrackerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTMovementTracker
+{
+    public class MovementTrackerConfiguration : IRocketPluginConfiguration
+    {
+        public int TrackDelay { get; set; }
+        public bool LogInConsole { get; set; }
+        public bool useWebhook { get; set; }
+        public string WebhookURL { get; set; }
+        public void LoadDefaults()
+        {
+            TrackDelay = 30;
+            LogInConsole = true;
+            useWebhook = false;
+            WebhookURL = "https://discordapp.com/api/webhooks/{webhook.id}/{webhook.api}";
+        }
+    }
+}
